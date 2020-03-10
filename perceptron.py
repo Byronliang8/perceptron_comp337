9e7903fe6a3df551a18d1137c3cc1beb3011ed97
sgylian2 <byronliang@outlook.com>
2020-03-10 18:53:24 +0000

    need to change(classify the label 2,2)

--- perceptron.py
+++ perceptron.py
@@ -1,69 +1,91 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 def readData(testData, trainData):
-    train_data = pd.read_table(trainData, delimiter=",")
-    test_data = pd.read_table(testData, delimiter=",")
-    x_train = train_data.iloc[:, 0:4]
-    y_train = train_data.iloc[:, 4]
+    test_data=np.loadtxt(testData, dtype=np.str, delimiter=',')
+    train_data=np.loadtxt(trainData, dtype=np.str, delimiter=',')
+    #train_data = pd.read_table(trainData, delimiter=",")
+    #test_data = pd.read_table(testData, delimiter=",")
+    x_train = train_data[:,0:4].astype(np.float)
+    y_train = train_data[:, 4]
 
-    x_test = test_data.iloc[:, 0:4]
-    y_test = test_data.iloc[:, 4]
+    x_test = test_data[:,0:4].astype(np.float)
+    y_test = test_data[:, 4]
+    x_train=np.c_[x_train,np.ones(x_train.shape[0])]# another is for bias
+    x_test=np.c_[x_test,np.ones(x_test.shape[0])]# another is for bias
+    #print(x_train)
     return (x_train,y_train,x_test,y_test)
 
+def unique_label(y):
+    return np.unique(y)
+
+
 def label_num(y):
-    return len(y.unique())
-
-
-def linearly_separable(x,w,b):
-    z=np.dot(w.T, x) + b
-    if z<0 :
-        return -1
-    if z>=0 :
-       return 1
+    return len(unique_label(y))
 
 # initialize_weights randomly
-def initialize_weights(dim):
-    w = np.zeros(dim)
-    b = 0
-    return w, b
+def initialize_weights(dim,num):
+    w = np.zeros((dim,num))
+    return w.T
 
-def perceptron_model(x,w,b,y,r):
-    for i in range(100):
-        output=linearly_separable(x,w,b)
-        error=err(y,output)
-        w=update(error,x,w,r)
+def perceptron_model(x,w,s=0):
+    z=np.dot(w, x.T)
+    z[z>0]=1
+    z[z<=0]=0
+    return z.T
 
-    return 0
+def train_model(x,y,s,num_echo):
+    w=initialize_weights(dim=(x.shape[1]),num=label_num(y)-1)
+
+    for i in range(num_echo):
+        for j in range(x.shape[0]):
+            input=np.array([x[j,]])
+            output=perceptron_model(input,w)
+            label=getLabel(y,j)
+            w=update(w,err(label,output),input,0.1)
+    return w
+
+def update(w,err,x,r):
+    return w+(r*np.dot(x.T,err)).T
 
 def err(label,output):
     return label-output
 
-def update(err,x,w,r):
-    w=w+err*x*r
-    return w
+def getLabel(y,index):
+    label_list=unique_label(y)
+    i = np.where(label_list==y[index])[0]
+    label=np.zeros(label_num(y)-1)
+    if i==0:
+        label[i]=1
+    elif i==label_num(y)-1:
+        label[i-1]=1
+    else:
+        label[i]=1
+        label[i-1]=1
+    return label
 
-# Prediction function
-def predict(W, b, X):
-    m = X.shape[1]
-    Y_pred = np.zeros((1, m))
-    W = W.reshape(X.shape[0], 1)
+def predicate(w,x,y):
+    output=perceptron_model(x,w)
+    #print(output)
+    for i in range(output.shape[0]):
+        out=output[i]
+        if out[0]==1:
+            if out[1]==0:
+                print("class-1")
+            elif  out[1]==1:
+                print("class-2")
+        elif out[0]==0 :
+            if out[1]==1:
+                print("class-3")
 
-    A = sigmoid(np.dot(W.T, X) + b)
+    return 0
 
-    for i in range(m):
-        Y_pred[0, i] = 1 if A[0, i] > 0.5 else 0
 
-    assert (Y_pred.shape == (1, m))
-
-    return Y_pred
 
 trainPath='data/train.data'
 testPath='data/test.data'
-x_train,y_train,x_test,y_test=readData(trainPath,testPath)
+x_train,y_train,x_test,y_test=readData(trainData=trainPath,testData=testPath)
 
-w,b=initialize_weights(4)
+w=train_model(x_train,y_train,0,100)
 
-#optimize(w,b,x_train,y_train,0.1)
-print(initialize_weights(4))
+predicate(w,x_test,y_test)
